4cb29ab12e86622a5b1caa4e8af294e8721a9fbc
Fernando Gurgel <fag.gurgel@gmail.com>
2020-06-13 22:29:01 -0400

    created login screen and its style

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5"
+};
---
+++ src/pages/SingIn/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg'
+import { Container, Content, Background } from './Styled'
+import Button from '../../components/Button'
+import Input from '../../components/Input'
+
+const SingIn: React.FC = () => (
+  <>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <form>
+          <h1> Faça seu logon</h1>
+          <Input
+            name="txtName"
+            icon={FiMail}
+            type="email"
+            placeholder="E-mail"
+          />
+          <Input
+            name="txtPassword"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button name="loginSubmit" type="submit">
+            Entrar
+          </Button>
+        </form>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  </>
+)
+
+export default SingIn
